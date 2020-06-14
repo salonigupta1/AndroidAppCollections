4cfec004bd2f92f1f01f600a67f1597a21ccbda9
iamsaloni <saloniguptasg23@gmail.com>
2020-06-14 19:30:47 +0530

    Add Animations App

---
+++ Animations/app/src/main/java/com/example/animations/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.animations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade (View view){
+        Log.i("Info", "Clicked");
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

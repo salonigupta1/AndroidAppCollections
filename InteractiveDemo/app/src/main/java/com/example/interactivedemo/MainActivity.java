05a8d75d6ee18bc8ac48d527df15d14fe4150c05
iamsaloni <saloniguptasg23@gmail.com>
2020-06-14 19:35:39 +0530

    Add Interactive Demo App

---
+++ InteractiveDemo/app/src/main/java/com/example/interactivedemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.interactivedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

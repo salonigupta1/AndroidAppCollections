b2c9ab9b25f4962a2c9416521cd31efd4ca85188
iamsaloni <saloniguptasg23@gmail.com>
2020-06-14 19:40:48 +0530

    Add VideoDemo App

---
+++ VideoDemo/app/src/main/java/com/example/videodemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.videodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+    }
+}

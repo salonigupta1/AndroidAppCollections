065657632aea041d9d6a5bcac69a3f459a48fdf2
iamsaloni <saloniguptasg23@gmail.com>
2020-06-14 19:32:55 +0530

    Add DownloadingWebContent

---
+++ DownloadingWebContent/app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.downloadingwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+
+                    data = reader.read();
+                }
+                return result;
+            } catch (Exception e){
+                e.printStackTrace();
+                return "Failed!";
+            }
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("https://zappycode.com/").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Result", result);
+    }
+}
